cac56e5e9c162f81b9f9382c29cffca93f0e8925
vjyashwin <vjyashwin@gmail.com>
2019-01-04 11:25:39 +0530

    first commit

---
+++ Car.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author Vijay Srinivasa Raghavan
+ *
+ */
+public class Car extends Vehicle {
+       
+    public String getIdPlate() {return idPlate;}
+
+    public void setIdPlate(String idPlate) {
+        this.idPlate = idPlate;
+    }
+   }
---
+++ Vehicle.java
@@ -0,0 +1,19 @@
+
+
+
+/**
+ * @author Vijay Srinivasa Raghavan
+ *
+ */
+public abstract class Vehicle{
+//abstract class of the all vehicle types
+
+    //instance variables to store common data of a vehicle
+    
+    public String idPlate;
+    
+    public abstract String getIdPlate();
+
+    public abstract void setIdPlate(String idPlate);
+
+}
